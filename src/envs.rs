278b5485526dd41d76e965043f981f035ef84a96
Eric Kidd <git@randomhacks.net>
2026-06-18 10:12:42 +0100

    Add typed DeclIdx<T> wrapper
    This will help keep IDs straight.

--- src/envs.rs
+++ src/envs.rs
@@ -6,6 +6,8 @@
 use std::{
     cell::{Ref, RefCell},
     collections::HashMap,
+    fmt,
+    marker::PhantomData,
     rc::Rc,
 };
 
@@ -15,6 +17,46 @@ use crate::{
     ast::grammar::Ident,
     errors::{DuplicateDeclarationError, UnknownIdentifierError},
 };
+
+pub struct DeclIdx<T> {
+    idx: usize,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> DeclIdx<T> {
+    /// Private method for creating a new ID.
+    fn new(idx: usize) -> Self {
+        Self {
+            idx,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Clone for DeclIdx<T> {
+    fn clone(&self) -> Self {
+        Self {
+            idx: self.idx,
+            _phantom: self._phantom,
+        }
+    }
+}
+
+impl<T> Copy for DeclIdx<T> {}
+
+impl<T> fmt::Debug for DeclIdx<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.idx.fmt(f)
+    }
+}
+
+impl<T> PartialEq for DeclIdx<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.idx == other.idx
+    }
+}
+
+impl<T> Eq for DeclIdx<T> {}
 
 pub struct DeclTableHandle<T> {
     decls: Rc<RefCell<Vec<T>>>,
@@ -29,19 +71,19 @@ impl<T> DeclTableHandle<T> {
     }
 
     /// Insert an entry and return its ID.
-    fn insert(&mut self, value: T) -> usize {
+    fn insert(&mut self, value: T) -> DeclIdx<T> {
         let mut decls = self.decls.borrow_mut();
         let index = decls.len();
         decls.push(value);
-        index
+        DeclIdx::new(index)
     }
 
     /// Get an entry by its ID.
-    fn get(&self, index: usize) -> Option<Ref<'_, T>> {
+    fn get(&self, index: DeclIdx<T>) -> Option<Ref<'_, T>> {
         // Some moderately obscure trickery to take a borrowed RefCell, and map
         // a function over it, maintaining the original borrow with the derived
         // value.
-        Ref::filter_map(self.decls.borrow(), |decls| decls.get(index)).ok()
+        Ref::filter_map(self.decls.borrow(), |decls| decls.get(index.idx)).ok()
     }
 
     /// Borrow our underlying storage (for iteration, etc).
@@ -58,14 +100,17 @@ impl<T> Clone for DeclTableHandle<T> {
     }
 }
 
-pub struct NameMap<'parent, T> {
+/// A map from [`Ident`] to values of type `T`. Supports nested child scopes.
+///
+/// An `IdentMap` and all its children share a single underlying set of indices.
+pub struct IdentMap<'parent, T> {
     decl_table: DeclTableHandle<T>,
-    parent: Option<&'parent NameMap<'parent, T>>,
-    idents: HashMap<Ident, usize>,
+    parent: Option<&'parent IdentMap<'parent, T>>,
+    idents: HashMap<Ident, DeclIdx<T>>,
 }
 
-/// Create a new [`NameEnv`].
-impl<T> NameMap<'static, T> {
+impl<T> IdentMap<'static, T> {
+    /// Create a new [`NameEnv`].
     pub fn new() -> Self {
         Self {
             decl_table: DeclTableHandle::new(),
@@ -75,11 +120,11 @@ impl<T> NameMap<'static, T> {
     }
 }
 
-impl<'parent, T> NameMap<'parent, T> {
+impl<'parent, T> IdentMap<'parent, T> {
     /// Create a child [`NameEnv`] which shares the same [`DeclTableHandle`].
     /// This is needed because WASM frequently requires declaring all locals (or whatever)
     /// at the top of a function, sharing a single set of indices.
-    pub fn child<'new_parent: 'parent>(&'new_parent self) -> NameMap<'new_parent, T> {
+    pub fn child<'new_parent: 'parent>(&'new_parent self) -> IdentMap<'new_parent, T> {
         Self {
             decl_table: self.decl_table.clone(),
             parent: Some(self),
@@ -106,9 +151,14 @@ impl<'parent, T> NameMap<'parent, T> {
     }
 
     /// Look up `ident`, returning `None` if it does not exist.
-    pub fn try_get<'a>(&'a self, ident: &Ident) -> Option<Ref<'a, T>> {
+    pub fn try_get<'a>(&'a self, ident: &Ident) -> Option<(DeclIdx<T>, Ref<'a, T>)> {
         if let Some(index) = self.idents.get(ident) {
-            self.decl_table.get(*index)
+            Some((
+                *index,
+                self.decl_table
+                    .get(index.clone())
+                    .expect("environment missing idx"),
+            ))
         } else if let Some(parent) = self.parent {
             parent.try_get(ident)
         } else {
@@ -117,7 +167,10 @@ impl<'parent, T> NameMap<'parent, T> {
     }
 
     /// Look up `ident`, returning an error if it does not exist.
-    pub fn get<'a>(&'a self, ident: &Ident) -> Result<Ref<'a, T>, UnknownIdentifierError> {
+    pub fn get<'a>(
+        &'a self,
+        ident: &Ident,
+    ) -> Result<(DeclIdx<T>, Ref<'a, T>), UnknownIdentifierError> {
         self.try_get(ident)
             .ok_or_else(|| UnknownIdentifierError::new(ident.to_owned()))
     }
@@ -133,11 +186,12 @@ mod tests {
 
     #[test]
     fn insert_and_get() {
-        let mut map = NameMap::new();
+        let mut map = IdentMap::new();
 
         // Insert and get.
         map.insert(ident("foo"), 1).unwrap();
-        assert_eq!(*map.get(&ident("foo")).unwrap(), 1);
+        assert_eq!(map.get(&ident("foo")).unwrap().0, DeclIdx::new(0));
+        assert_eq!(*map.get(&ident("foo")).unwrap().1, 1);
 
         // Duplicate insert.
         assert!(map.insert(ident("foo"), 5).is_err());
@@ -147,17 +201,17 @@ mod tests {
 
         // Create child.
         let mut child = map.child();
-        assert_eq!(*child.get(&ident("foo")).unwrap(), 1);
+        assert_eq!(*child.get(&ident("foo")).unwrap().1, 1);
 
         // Insert into child.
         child.insert(ident("foo"), 3).unwrap();
-        assert_eq!(*child.get(&ident("foo")).unwrap(), 3);
+        assert_eq!(*child.get(&ident("foo")).unwrap().1, 3);
 
         // Verify parent is unaffected.
-        assert_eq!(*map.get(&ident("foo")).unwrap(), 1);
+        assert_eq!(*map.get(&ident("foo")).unwrap().1, 1);
 
         // Make sure other key unshadowed in child.
-        assert_eq!(*child.get(&ident("bar")).unwrap(), 2);
+        assert_eq!(*child.get(&ident("bar")).unwrap().1, 2);
 
         // Verify we have the right entries.
         let decls = map.decl_table().decls();
