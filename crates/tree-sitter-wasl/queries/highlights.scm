edfba5b568b42fbc0168ef26213eab888be0c50f
Eric Kidd <git@randomhacks.net>
2026-06-24 17:15:32 +0100

    Update syntax highlighting

--- crates/tree-sitter-wasl/queries/highlights.scm
+++ crates/tree-sitter-wasl/queries/highlights.scm
@@ -1,17 +1,23 @@
-["import" "export" "func"] @keyword
+["import" "export" "func" "mut" "null" "record"] @keyword
 
-(_val_type) @type
+["i8" "u8" "i32" "u32"] @type.builtin
+
+(ident) @variable
 (number) @number
 (func_sig name: (ident) @function)
+(param name: (ident) @variable.parameter)
+(linear_field name: (ident) @property)
 (comment) @comment
 
-["{" "}"] @punctuation.bracket
-
-["(" ")"] @punctuation.bracket
+["{" "}" "(" ")"] @punctuation.bracket
 
 [
   ","
+  ":"
+  ";"
   "->"
 ] @punctuation.delimiter
+
+["+" "*"] @operator
 
 ;;(ERROR) @error
